6be199d9070876418a68e7edfc7255063cd9ac02
yusuke mitsugi <pastabal.jungle@gmail.com>
2020-07-25 11:02:28 +0900

    Initial Commit

---
+++ ShimmerAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShimmerAnimation
+//
+//  Created by Yusuke Mitsugi on 2020/07/25.
+//  Copyright © 2020 Yusuke Mitsugi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
